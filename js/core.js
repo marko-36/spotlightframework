e2a5c9b9f8995b79a32e0f7cead05bafc7873231
marko-36 <26520303+marko-36@users.noreply.github.com>
2021-08-04 19:50:44 +0200

    Removing jQuery dependency

--- js/core.js
+++ js/core.js
@@ -1,61 +1,52 @@
 ﻿var body = document.querySelector("body");
 
-    /*-- toggle body.navmain-active 2021-06-06 --*/
+// body.navmain-active toggle 2021-06-06
+document.querySelector('a._amenu').addEventListener("click", menuToggle);
+body.addEventListener("click", menuToggle);
 
-document.querySelector('._amenu').addEventListener("click", (ev) => {
-	ev.stopImmediatePropagation();
-	body.classList.toggle('navmain-active');
-});
-
-    /*-- nav_main hover 2021-06-14 --*/
-$(document).on('mouseover mouseout click','li._node', function(ev){
+function menuToggle(ev) {"use strict";
 	ev.preventDefault();
-	ev.stopImmediatePropagation();
-	if ($(this).hasClass('navhover')){
-		if ((window.inputMode == 'mouse' && ev.type == 'mouseout') || ev.type == 'click'){$(this).removeClass('navhover')}
-	} else {
-		if ((window.inputMode == 'mouse' && ev.type == 'mouseover') || ev.type == 'click'){$(this).addClass('navhover')}
-	}
-})
-
-$(document).on('click','a._amenu, body.navmain-active',(function(event) {"use strict";
-	event.preventDefault();
-	if ($('body').hasClass('navmain-active')) {
-		$('body').removeClass('navmain-active');        
-		$('#nav_main').find('li').removeClass('navhover');		
+	if (body.classList.contains('navmain-active')){
+		document.querySelectorAll('li._node').forEach(menuNode => {
+			menuNode.classList.remove('navhover');
+		})
+		body.classList.remove('navmain-active');        			
 		/*if($(window).scrollTop()>0){$('body').addClass('navmain-hidden');$('nav').css('top', '');}*/
 	} else {
-		$('body').addClass('navmain-active');
+		body.classList.add('navmain-active');
 		/*$('nav').css('top', $(window).scrollTop());*/
 	}
-	event.stopImmediatePropagation();	
-}));
+	ev.stopImmediatePropagation();	
+};
 
-/*-- NAV MOBILE #NAV_MAIN NAVHOVERs 2018-03-15 A --*/
-/*var moves=0;
-var validmouseleave = true;
-$(document).on('mouseleave','#nav_main', (function (event) {"use strict";
-	if (validmouseleave === true){
-		$('nav li.navhover').removeClass('navhover');
-	}else{
-		event.stopImmediatePropagation();
+// nav_main li._node toggle 2021-08-04
+document.querySelectorAll('li._node').forEach(menuNode => {
+	menuNode.addEventListener('click', menuNodeToggle)
+	menuNode.addEventListener('mouseover', menuNodeToggle)
+	menuNode.addEventListener('mouseout', menuNodeToggle)		
+})	
+
+function menuNodeToggle(ev) {
+	ev.preventDefault();
+	ev.stopImmediatePropagation();
+	if ((ev.currentTarget.classList.contains('navhover')) && ((window.inputMode == 'mouse' && ev.type == 'mouseout') || (ev.type == 'click'))) {
+		ev.currentTarget.classList.remove('navhover')
+	} else if ((window.inputMode == 'mouse' && ev.type == 'mouseover') || (ev.type == 'click')){
+		var otherNodes = Array.prototype.slice.call(ev.currentTarget.parentNode.children);
+		otherNodes.forEach(node => {
+			node.classList.remove('navhover')
+		});
+		ev.currentTarget.classList.add('navhover');	
 	}
-}));
-$(document).on('mousemove', '#nav_main a', (function (event) {"use strict";
-    if($(this).parent().parents().find('nav .navhover').lenght===0){	//deaktivuj předchůdce, pokud nemají navhover
-    $('nav li.navhover').removeClass('navhover');}
-    $(this).parent().siblings().removeClass('navhover');	//deaktivuj sourozence
-    $(this).parent().find('li').removeClass('navhover');	//deaktivuj děti
-    $(this).parent().addClass('navhover');
-    event.stopImmediatePropagation();
-	}
-));
-*/
+}
 
-//-- DEFAULT .on('click','html') 2016-03-30 A --*/
-
-$(document).on('click','html',(function() {"use strict";
-	$('#nav_main').find('li.navhover').removeClass('navhover');
-	$('body').removeClass('_sided');
-	if($(window).scrollTop()>0){$('body').addClass('navmain-hidden').css('top', '');}
-}));
+// DEFAULT .on('click','html') 2016-03-30 A
+// $(document).on('click','html',(function() {
+document.querySelector('html').addEventListener("click", htmlReset);
+function htmlReset(){
+	document.querySelectorAll('li._node').forEach(menuNode => {
+		menuNode.classList.remove('navhover');
+	})
+	body.classList.remove('_sided');
+	//if($(window).scrollTop()>0){$('body').addClass('navmain-hidden').css('top', '');}
+};
