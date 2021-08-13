061ad4a394c5a3d00d3dc118048a08feae1cbd06
marko-36 <26520303+marko-36@users.noreply.github.com>
2021-08-14 00:30:10 +0200

    scrolled-by-x jQuery dependence removed

--- js/scrolled-by-x.js
+++ js/scrolled-by-x.js
@@ -1,6 +1,6 @@
 window.lastScrollDir = "up";
 window.lastScrollTop = window.pageYOffset;
-window.scrollTreshold = 10; //
+window.scrollTreshold = 10;
 
 isBigScroll = (lastScrollTop, scrollTreshold) => {
   return (Math.abs(window.pageYOffset - lastScrollTop) > scrollTreshold) ? true : false
@@ -10,19 +10,19 @@ setScrollDir = (lastScrollTop) => {
   if (window.pageYOffset > lastScrollTop) {
     window.lastScrollDir = "down";
     console.log("down");
-    $('body').addClass('scrolledDown');
+    body.classList.add('scrolledDown');
     return "down"
   } else {
     window.lastScrollDir = "up";
     console.log("up");    
-    $('body').removeClass('scrolledDown');
+    body.classList.remove('scrolledDown');  
     return "up"
   }
 }
 
-$(window).scroll(function() {
-  if (isBigScroll(lastScrollTop, 10)) {
+ window.onscroll = function() {    
+  if (isBigScroll(lastScrollTop, window.scrollTreshold)) {
     setScrollDir(window.lastScrollTop);
     window.lastScrollTop = window.pageYOffset;
   }
-});
+};
